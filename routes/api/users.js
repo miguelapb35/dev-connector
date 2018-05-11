a5cf5848e0cafd193b5cb3659e48c978b8830af0
Chris Kew <ckew@oculus.co.uk>
2018-05-11 13:15:31 +0100

    Added register route and added user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,57 @@
 /* This is the users routes */
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
+// Load the user model
+const User = require("../../models/User");
+
+// @route   GET api/users/test
+// @desc    Tests users route
+// @access  Public
 router.get("/test", (req, res) => {
     return res.json({
         message: "Users works"
     });
+});
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post("/register", (req, res) => {
+    User.findOne({
+        email: req.body.email
+    }).then(user => {
+        if (user) {
+            return res.status(400).json({
+                email: "Sorry, email already exists!"
+            });
+        } else {
+            const avatar = gravatar.url(req.body.email, {
+                size: "200", // Size
+                rating: "pg", // Rating
+                default: "mm" // Default
+            });
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password
+            });
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) {
+                        throw err;
+                    }
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                })
+            });
+        }
+    })
 });
 
 module.exports = router;
