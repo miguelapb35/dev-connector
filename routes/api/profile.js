c93f5a62be6f634362ddf81e089640cd75cd078f
Chris Kew <chris.kew@gmail.com>
2018-05-20 23:45:00 +0100

    Back-end api complete, added React front-end boilerplate code ready to start front-end build

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -250,7 +250,7 @@ router.post("/education", passport.authenticate("jwt", { session: false }), (req
 // @route   Delete api/profile/experience/:exp_id
 // @desc    Delete experience from profile
 // @access  Private
-router.post("/experience/:exp_id", passport.authenticate("jwt", { session: false }), (req, res) => {
+router.delete("/experience/:exp_id", passport.authenticate("jwt", { session: false }), (req, res) => {
 
     Profile.findOne({
             user: req.user.id
@@ -271,6 +271,51 @@ router.post("/experience/:exp_id", passport.authenticate("jwt", { session: false
                 });
         })
         .catch(err => res.status(404).json(err));
+});
+
+// @route   Delete api/profile/education/:edu_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete("/education/:edu_id", passport.authenticate("jwt", { session: false }), (req, res) => {
+
+    Profile.findOne({
+            user: req.user.id
+        })
+        .then(profile => {
+            // Get remove index
+            const removeIndex = profile.education
+                .map(item => item.id)
+                .indexOf(req.params.edu_id);
+
+            // Splice out of array
+            profile.education.splice(removeIndex, 1);
+
+            // Save profile
+            profile.save()
+                .then(profile => {
+                    return res.json(profile);
+                });
+        })
+        .catch(err => res.status(404).json(err));
+});
+
+// @route   Delete api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete("/", passport.authenticate("jwt", { session: false }), (req, res) => {
+
+    Profile.findOneAndRemove({
+        user: req.user.id
+    }).then(() => {
+        User.findOneAndRemove({
+                _id: req.user.id
+            })
+            .then(() => {
+                return res.json({
+                    success: true
+                });
+            });
+    })
 });
 
 module.exports = router;
